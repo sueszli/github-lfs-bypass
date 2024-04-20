65d1b9077e557ba942f597ed55eaa22cb6cad265
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-04-20 04:39:48 +0200

    Delete clone.sh

--- clone.sh
+++
@@ -1,9 +0,0 @@
-# merge data chunks into data-merged directory (they're chunked to bypass git-lfs)
-chmod +x lfs-merge.sh
-./lfs-merge.sh
-
-# untar data-merged/merged.tar into data-merged
-tar -xf data-merged/merged.tar -C data-merged
-
-# remove data-merged/merged.tar
-rm -f data-merged/merged.tar
